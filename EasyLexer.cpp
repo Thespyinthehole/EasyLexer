c73f4d2351a6355bf39872ee8282e1658598b1be
Thespyinthehole <joshuaaubrey@talktalk.net>
2020-08-11 11:36:20 +0100

    Made it faster

--- EasyLexer.cpp
+++ EasyLexer.cpp
@@ -54,7 +54,7 @@ Token::Token(int line, int char_start, std::string value)
 void EasyLexer::add_new_token(Tokens token, std::string regex)
 {
     //Add a new valid token to the map, turning the regex string into a regex object
-    tokens.insert(std::pair<Tokens, std::regex>(token, std::regex(regex)));
+    tokens.insert(std::pair<Tokens, std::regex>(token, std::regex("^" + regex)));
 }
 
 LexicalException::LexicalException(std::vector<Token> errors)
@@ -124,93 +124,58 @@ Token EasyLexer::next_token()
     //Get the iterator for the map of valid tokens
     std::map<Tokens, std::regex>::iterator iter;
 
+    std::string current_analysis = string_to_analysis.substr(current_char_location);
+    std::smatch regex_match;
+
     //Loop over each valid token
     for (iter = tokens.begin(); iter != tokens.end(); iter++)
     {
-        //How many characters should we read
-        int offset = 1;
-
-        //Has this token matched any characters yet
-        bool found = false;
-
-        //Store current line and character position
-        int current_line = line_number;
-        int current_char = char_position;
-
-        //The current search string
-        std::string current_analysis = string_to_analysis.substr(current_char_location, offset);
-
-        //Loop over the rest of the characters
-        while (current_char_location + offset <= string_to_analysis.size())
-        {
-            //If we have a match already, keep searching until it doesnt match
-            if (found)
-            {
-                if (!std::regex_match(current_analysis, iter->second))
-                {
-                    current_analysis = string_to_analysis.substr(current_char_location, --offset);
-                    break;
-                }
-            }
-            //If we just found a match note it
-            else if (std::regex_match(current_analysis, iter->second))
-                found = true;
-
-            //If we have reached a \n move line number on 1 and reset char, otherwise increment current char
-            if (string_to_analysis.at(current_char_location + offset - 1) == '\n')
-            {
-                current_line++;
-                current_char = 1;
-            }
-            else
-                current_char++;
-            //Update the current search string
-            current_analysis = string_to_analysis.substr(current_char_location, ++offset);
-        }
-
-        //If a match has been found return a token object with the correct type and value.
-        if (found)
-        {
-            //If you reached the second to last character it kinda messes up, but this solves it
-            if ((current_char_location + offset) == string_to_analysis.size())
-                current_analysis = string_to_analysis.substr(current_char_location, --offset);
-
-            //Move along the number of characters we used
-            current_char_location += offset;
-
-            //If an error was found add it to the list
-            if (error_characters.size() > 0)
-                errors.push_back(Token(error_line_number, error_char_position, error_characters));
-
-            //Reset the error string
-            error_characters = "";
-
-            //Add the successful token
-            Token token = Token(iter->first, line_number, char_position, current_analysis);
-
-            //Move the line and char position along
-            line_number = current_line;
-            char_position = current_char;
-            error_char_position = char_position;
-            error_line_number = line_number;
-            return token;
-        }
+        //Check if we match
+        if (std::regex_search(current_analysis, regex_match, iter->second))
+            break;
     }
 
-    //If we have reached a \n move line number on 1 and reset char, otherwise increment current char
+    if (regex_match.size() != 0)
+    {
+        if (error_characters.size() > 0)
+            errors.push_back(Token(error_line_number, error_char_position, error_characters));
+
+        //Reset the error string
+        error_characters = "";
+
+        //Get the matched string and return a token linking to it
+        std::string matched_string = regex_match[0].str();
+        current_char_location += matched_string.size();
+        Token token(iter->first, line_number, char_position, matched_string);
+
+        //Move the line and char position along
+        for (char chr : matched_string) {
+            if (chr == '\n')
+            {
+                line_number++;
+                char_position = 1;
+            }
+            else
+                char_position++;
+        }
+        error_char_position = char_position;
+        error_line_number = line_number;
+        return token;
+    }
+
+
     if (string_to_analysis.at(current_char_location) == '\n')
     {
         line_number++;
-        char_position = 0;
+        char_position = 1;
     }
     else
         char_position++;
 
-    //Add the invalid character to the error string
+    //Didnt find a match so add an invalid character and try from next character
     error_characters += string_to_analysis.at(current_char_location++);
 
     if (current_char_location == string_to_analysis.size())
         errors.push_back(Token(error_line_number, error_char_position, error_characters));
-    //Look for other succesful tokens
     return next_token();
 }
